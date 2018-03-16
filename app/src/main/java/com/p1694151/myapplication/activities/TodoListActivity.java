41b928da25c871962a759282aedb2402e6d75d65
paalwindersingh <paalwinder.619@gmail.com>
2018-03-16 11:15:52 +0530

    Added date picker and radio button

--- app/src/main/java/com/p1694151/myapplication/activities/TodoListActivity.java
+++ app/src/main/java/com/p1694151/myapplication/activities/TodoListActivity.java
@@ -1,22 +1,24 @@
 package com.p1694151.myapplication.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.p1694151.myapplication.R;
+import com.p1694151.myapplication.models.TodoItem;
 import com.p1694151.myapplication.utils.CustomButton;
 
 /**
  * Created by paalwinder on 15/02/18.
  */
 
-public class TodoListActivity extends AppCompatActivity{
+public class TodoListActivity extends AppCompatActivity {
 
     private CustomButton buttonCreate;
+    private TodoItem todoItem;
+
+    private boolean isEditView = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +26,13 @@ public class TodoListActivity extends AppCompatActivity{
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        isEditView = getIntent().getBooleanExtra("isEditView", false);
+        if (isEditView) {
+            todoItem = getIntent().getParcelableExtra("todo");
+        }
+
         buttonCreate = (CustomButton) findViewById(R.id.button_create);
+        buttonCreate.setText(isEditView ? "Update" : "Create");
 
         buttonCreate.setOnItemClickListener(new CustomButton.OnItemClickListener() {
             @Override
--- app/src/main/java/com/p1694151/myapplication/adapter/TodoListAdapter.java
+++ app/src/main/java/com/p1694151/myapplication/adapter/TodoListAdapter.java
@@ -1,11 +1,15 @@
 package com.p1694151.myapplication.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.p1694151.myapplication.R;
+import com.p1694151.myapplication.activities.DrawerActivity;
+import com.p1694151.myapplication.activities.TodoListActivity;
 import com.p1694151.myapplication.models.TodoItem;
 import com.p1694151.myapplication.viewholder.TodoItemHolder;
 
@@ -21,9 +25,11 @@ public class TodoListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     private ArrayList<TodoItem> itemList = new ArrayList<>();
     private int width;
+    private Context context;
 
-    public TodoListAdapter(ArrayList<TodoItem> itemList) {
+    public TodoListAdapter(ArrayList<TodoItem> itemList, Context context) {
         this.itemList = itemList;
+        this.context = context;
     }
 
     @Override
@@ -54,6 +60,15 @@ public class TodoListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
     private void bindInvetmentView(final TodoItem item, TodoItemHolder itemListViewHolder) {
         itemListViewHolder.tvTitle.setText(item.getTitle());
         itemListViewHolder.tvNotes.setText(item.getNotes());
+        itemListViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, TodoListActivity.class);
+                intent.putExtra("isEditView", true);
+                intent.putExtra("todo", item);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
